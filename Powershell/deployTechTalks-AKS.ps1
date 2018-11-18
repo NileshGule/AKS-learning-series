52bb743f1944c83e28cb44ad8d880eabcbe7a77e
Nilesh Gule <vn_nilesh@yahoo.com>
2018-11-18 16:54:30 +0800

    removed helpers file, cleanup in pvc file

--- Powershell/deployTechTalks-AKS.ps1
+++ Powershell/deployTechTalks-AKS.ps1
@@ -17,23 +17,23 @@ Write-Host "Creating namespace"  -ForegroundColor Yellow
 kubectl apply --filename 00_AKS_Namespace.yml
 Write-Host "Namespace created successfully" -ForegroundColor Cyan
 
-Write-Host "Creating storage class" -ForegroundColor Yellow
-kubectl apply --filename 01_StorageClass.yml
-Write-Host "Storage class created successfully" -ForegroundColor Cyan
+# Write-Host "Creating storage class" -ForegroundColor Yellow
+# kubectl apply --filename 01_StorageClass.yml
+# Write-Host "Storage class created successfully" -ForegroundColor Cyan
 
-Write-Host "Creating Persistant Volume Claim" -ForegroundColor Yellow
-kubectl apply --filename 02_PersistantVolumeClaim.yml
-Write-Host "Persistant Volume Claim created successfully" -ForegroundColor Cyan
+# Write-Host "Creating Persistant Volume Claim" -ForegroundColor Yellow
+# kubectl apply --filename 02_PersistantVolumeClaim.yml
+# Write-Host "Persistant Volume Claim created successfully" -ForegroundColor Cyan
 
-Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
-Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB
-kubectl apply --recursive --filename . 
+# Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
+# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB
+# kubectl apply --recursive --filename . 
 
 Write-Host "Tech talks DB service deployed successfully" -ForegroundColor Cyan
 
-Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
-Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
-kubectl apply --recursive --filename . 
+# Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
+# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
+# kubectl apply --recursive --filename . 
 
 Write-Host "Tech talks API service deployed successfully" -ForegroundColor Cyan
 
