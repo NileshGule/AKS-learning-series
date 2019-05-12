bf8d19e880ff48dac9c2e74463470158715f06cf
Nilesh Gule <vn_nilesh@yahoo.com>
2019-05-12 15:13:36 +0800

    formatted deployment script

--- Powershell/deployTechTalks-AKS.ps1
+++ Powershell/deployTechTalks-AKS.ps1
@@ -1,6 +1,8 @@
 Param(
     [parameter(Mandatory=$false)]
-    [bool]$ProvisionAKSCluster=$false
+    [bool]$ProvisionAKSCluster=$false,
+    [parameter(Mandatory=$false)]
+    [string]$NamespaceName="ag1"
 )
 
 
@@ -21,21 +23,38 @@ Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB2019
 
 Write-Host "Deploying SQL Server Operator" -ForegroundColor Yellow
-kubectl apply --filename 00_operator.yml 
-kubectl apply -f 00_operator.yml --namespace ag1
+# kubectl apply --filename 00_operator.yml 
+kubectl apply `
+--filename 00_operator.yml `
+--namespace $NamespaceName
+
 Write-Host "SQL Server Operator deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Creating SA password" -ForegroundColor Yellow
-kubectl apply --filename 01_sa-password.yml
-kubectl create secret generic sql-secrets --from-literal=sapassword="P@ssw0rd!" --from-literal=masterkeypassword="P@ssw0rd!"  --namespace ag1
+# kubectl apply --filename 01_sa-password.yml
+kubectl create `
+secret generic sql-secrets `
+--from-literal=sapassword="P@ssw0rd!" `
+--from-literal=masterkeypassword="P@ssw0rd!"  `
+--namespace $NamespaceName
+
 Write-Host "Storage class created successfully" -ForegroundColor Cyan
 
-Write-Host "Deploying SQL Server custome resource" -ForegroundColor Yellow
-kubectl apply --filename 04_sqlserver.yml
-kubectl apply -f .\storage-class.yml --namespace ag1
-kubectl apply -f .\persistant-volume.yml --namespace ag1
-kubectl apply -f 04_sqlserver.yml --namespace ag1
-Write-Host "SQL Server custome resource deployed successfully" -ForegroundColor Cyan
+Write-Host "Deploying SQL Server custom resource" -ForegroundColor Yellow
+# kubectl apply --filename 04_sqlserver.yml
+kubectl apply `
+--filename 04_sqlserver.yml `
+--namespace $NamespaceName
+
+Write-Host "SQL Server custom resource deployed successfully" -ForegroundColor Cyan
+
+Write-Host "Deploying SQL Server Availability Group" -ForegroundColor Yellow
+# kubectl apply --filename 04_sqlserver.yml
+kubectl apply `
+--filename 04_agservices.yml `
+--namespace $NamespaceName
+
+Write-Host "SQL Server Availability Group deployed successfully" -ForegroundColor Cyan
 
 
 Write-Host "Tech talks DB service deployed successfully" -ForegroundColor Cyan
@@ -48,7 +67,11 @@ Write-Host "Tech talks DB service deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks web frontend" -ForegroundColor Yellow
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksWeb
-kubectl apply --recursive --filename . 
+
+kubectl apply `
+--recursive `
+--filename . `
+--namespace $NamespaceName
 
 Write-Host "Tech talks web frontend deployed successfully" -ForegroundColor Cyan
 
