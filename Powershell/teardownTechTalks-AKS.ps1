2f758343b894f49a7352706b89429d37601a3c67
Nilesh Gule <vn_nilesh@yahoo.com>
2019-05-12 23:04:20 +0800

    Added namespace to kubectl

--- Powershell/teardownTechTalks-AKS.ps1
+++ Powershell/teardownTechTalks-AKS.ps1
@@ -1,27 +1,37 @@
+Param(
+    [parameter(Mandatory=$false)]
+    [string]$NamespaceName="ag1"
+)
+
 Write-Host "Starting deletion of TechTalks application and services" -ForegroundColor Yellow
 
 Write-Host "Deleting Tech Talks web frontend" -ForegroundColor Yellow
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksWeb
-kubectl delete --recursive --filename . 
+kubectl delete --recursive --filename . `
+--namespace $NamespaceName
+
 
 Write-Host "Tech talks web frontend deleted successfully" -ForegroundColor Cyan
 
 Write-Host "Deleting Tech Talks API service" -ForegroundColor Yellow
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
-kubectl delete --recursive --filename . 
+kubectl delete --recursive --filename . `
+--namespace $NamespaceName
 
 Write-Host "Tech talks API service deleted successfully" -ForegroundColor Cyan
 
 Write-Host "Deleting Tech Talks DB service" -ForegroundColor Yellow
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB2019
-kubectl delete --recursive --filename . 
+kubectl delete --recursive --filename . `
+--namespace $NamespaceName
 
 Write-Host "Tech talks DB service deleted successfully" -ForegroundColor Cyan
 
 Set-Location ~/projects/AKS-learning-series/k8s/AKS/
 
 Write-Host "Deleting namespace"  -ForegroundColor Yellow
-kubectl delete --filename 00_AKS_Namespace.yml
+kubectl delete --filename 00_AKS_Namespace.yml `
+--namespace $NamespaceName
 Write-Host "Namespace deleted successfully" -ForegroundColor Cyan
 
 Write-Host "All the services related to Tech Talks application have been successfully deleted" -ForegroundColor Cyan
