4f0af6beee22514717db0fe04a003c7d1d06823f
Nilesh Gule <vn_nilesh@yahoo.com>
2019-05-21 00:53:41 +0800

    updated manifest files

--- Powershell/deployTechTalks-AKS.ps1
+++ Powershell/deployTechTalks-AKS.ps1
@@ -12,38 +12,45 @@ if($ProvisionAKSCluster)
     & ((Split-Path $MyInvocation.InvocationName) + "\initializeAKS.ps1") 
 }
 
+$currentWorkingDirectory = (Get-Location).Path | Split-Path -Parent
+$aksRootDirectory = Join-Path $currentWorkingDirectory "k8s/AKS"
+$techTalksDB2019RootDirectory = Join-Path $aksRootDirectory "TechTalksDB2019"
+$techTalksAPIRootDirectory = Join-Path $aksRootDirectory "TechTalksAPI"
+$techTalksWebRootDirectory = Join-Path $aksRootDirectory "TechTalksWeb"
+
 Write-Host "Starting deployment of TechTalks application and services" -ForegroundColor Yellow
-Set-Location ~/projects/AKS-learning-series/k8s/AKS/
+
+Set-Location $aksRootDirectory
 
 Write-Host "Creating namespace"  -ForegroundColor Yellow
 kubectl apply --filename 00_AKS_Namespace.yml
 Write-Host "Namespace created successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
-Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB2019
+Set-Location $techTalksDB2019RootDirectory
 
 Write-Host "Deploying SQL Server Operator" -ForegroundColor Yellow
-# kubectl apply --filename 00_operator.yml 
+
 kubectl apply `
---filename 00_operator.yml `
+--filename operator.yml `
 --namespace $NamespaceName
 
 Write-Host "SQL Server Operator deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Creating SA password" -ForegroundColor Yellow
-# kubectl apply --filename 01_sa-password.yml
+
 kubectl create `
 secret generic sql-secrets `
 --from-literal=sapassword="P@ssw0rd!" `
 --from-literal=masterkeypassword="P@ssw0rd!"  `
 --namespace $NamespaceName
 
-Write-Host "Storage class created successfully" -ForegroundColor Cyan
+# Write-Host "Storage class created successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying SQL Server custom resource" -ForegroundColor Yellow
-# kubectl apply --filename 04_sqlserver.yml
+
 kubectl apply `
---filename 04_sqlserver.yml `
+--filename sqlserver.yml `
 --namespace $NamespaceName
 
 Write-Host "SQL Server custom resource deployed successfully" -ForegroundColor Cyan
@@ -51,7 +58,7 @@ Write-Host "SQL Server custom resource deployed successfully" -ForegroundColor C
 Write-Host "Deploying SQL Server Availability Group" -ForegroundColor Yellow
 
 kubectl apply `
---filename 05-agservices.yml `
+--filename ag-services.yml `
 --namespace $NamespaceName
 
 Write-Host "SQL Server Availability Group deployed successfully" -ForegroundColor Cyan
@@ -59,14 +66,19 @@ Write-Host "SQL Server Availability Group deployed successfully" -ForegroundColo
 
 Write-Host "Tech talks DB service deployed successfully" -ForegroundColor Cyan
 
-# Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
-# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
-# kubectl apply --recursive --filename . 
+Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
 
-# Write-Host "Tech talks API service deployed successfully" -ForegroundColor Cyan
+# Set-Location $techTalksAPIRootDirectory
+
+# kubectl apply `
+# --recursive `
+# --filename . `
+# --namespace $NamespaceName 
+
+Write-Host "Tech talks API service deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks web frontend" -ForegroundColor Yellow
-Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksWeb
+Set-Location $techTalksWebRootDirectory
 
 kubectl apply `
 --recursive `
