1dab3312a097573066099338794be8a82c802cfd
Nilesh Gule <vn_nilesh@yahoo.com>
2019-05-29 23:31:05 +0800

    Added Istio Manifest files

--- Powershell/initializeAKS.ps1
+++ Powershell/initializeAKS.ps1
@@ -1,19 +1,19 @@
 Param(
-    [parameter(Mandatory=$false)]
-    [string]$subscriptionName="Microsoft Azure Sponsorship",
-    [parameter(Mandatory=$false)]
-    [string]$resourceGroupName="techtalksrg",
-    [parameter(Mandatory=$false)]
-    [string]$resourceGroupLocaltion="South East Asia",
-    [parameter(Mandatory=$false)]
-    [string]$clusterName="techtalksCluster",
-    [parameter(Mandatory=$false)]
-    [string]$dnsNamePrefix="techtalksdns",
-    [parameter(Mandatory=$false)]
-    [int16]$workerNodeCount=4, #minimum 4 nodes required for SQL 2019 HA setup
-    [parameter(Mandatory=$false)]
-    [string]$kubernetesVersion="1.13.5"
-    
+    [parameter(Mandatory = $false)]
+    [string]$subscriptionName = "Microsoft Azure Sponsorship",
+    [parameter(Mandatory = $false)]
+    [string]$resourceGroupName = "techtalksrg",
+    [parameter(Mandatory = $false)]
+    [string]$resourceGroupLocaltion = "South East Asia",
+    [parameter(Mandatory = $false)]
+    [string]$clusterName = "techtalksCluster",
+    [parameter(Mandatory = $false)]
+    [string]$dnsNamePrefix = "techtalksdns",
+    [parameter(Mandatory = $false)]
+    [int16]$workerNodeCount = 4, #minimum 4 nodes required for SQL 2019 HA setup
+    [parameter(Mandatory = $false)]
+    [string]$kubernetesVersion = "1.13.5"
+
 )
 
 # Set Azure subscription name
@@ -23,27 +23,27 @@ az account set --subscription=$subscriptionName
 # Create resource group name
 Write-Host "Creating resource group $resourceGroupName in region $resourceGroupLocaltion" -ForegroundColor Yellow
 az group create `
---name=$resourceGroupName `
---location=$resourceGroupLocaltion `
---output=jsonc
+    --name=$resourceGroupName `
+    --location=$resourceGroupLocaltion `
+    --output=jsonc
 
 # Create AKS cluster
 Write-Host "Creating AKS cluster $clusterName with resource group $resourceGroupName in region $resourceGroupLocaltion" -ForegroundColor Yellow
 az aks create `
---resource-group=$resourceGroupName `
---name=$clusterName `
---node-count=$workerNodeCount `
---dns-name-prefix=$dnsNamePrefix `
---generate-ssh-keys `
---node-vm-size=Standard_D2_v2 `
---kubernetes-version=$kubernetesVersion `
---enable-addons http_application_routing
+    --resource-group=$resourceGroupName `
+    --name=$clusterName `
+    --node-count=$workerNodeCount `
+    --dns-name-prefix=$dnsNamePrefix `
+    --generate-ssh-keys `
+    --node-vm-size=Standard_D2_v2 `
+    --kubernetes-version=$kubernetesVersion `
+    --enable-addons http_application_routing
 
 # Get credentials for newly created cluster
 Write-Host "Getting credentials for cluster $clusterName" -ForegroundColor Yellow
 az aks get-credentials `
---resource-group=$resourceGroupName `
---name=$clusterName
+    --resource-group=$resourceGroupName `
+    --name=$clusterName
 
 Write-Host "Successfully created cluster $clusterName with kubernetes version $kubernetesVersion and $workerNodeCount node(s)" -ForegroundColor Green
 
