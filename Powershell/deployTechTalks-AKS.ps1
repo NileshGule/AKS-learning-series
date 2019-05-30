3088cc91123752a1a848d95ddb170960eb17afc6
Nilesh Gule <vn_nilesh@yahoo.com>
2019-05-30 15:11:27 +0800

    Removed hardcoded namespace name from the manifest files

--- Powershell/deployTechTalks-AKS.ps1
+++ Powershell/deployTechTalks-AKS.ps1
@@ -1,13 +1,12 @@
 Param(
-    [parameter(Mandatory=$false)]
-    [bool]$ProvisionAKSCluster=$false
+    [parameter(Mandatory = $false)]
+    [bool]$ProvisionAKSCluster = $false
 )
 
 
-if($ProvisionAKSCluster)
-{
+if ($ProvisionAKSCluster) {
     Write-Host "Provisioning AKS cluster with default parameters" -ForegroundColor Cyan
-    & ((Split-Path $MyInvocation.InvocationName) + "\initializeAKS.ps1") 
+    & ((Split-Path $MyInvocation.InvocationName) + "\initializeAKS.ps1")
 }
 
 Write-Host "Starting deployment of TechTalks application and services" -ForegroundColor Yellow
@@ -17,31 +16,31 @@ Write-Host "Creating namespace"  -ForegroundColor Yellow
 kubectl apply --filename 00_AKS_Namespace.yml
 Write-Host "Namespace created successfully" -ForegroundColor Cyan
 
-# Write-Host "Creating storage class" -ForegroundColor Yellow
-# kubectl apply --filename 01_StorageClass.yml
-# Write-Host "Storage class created successfully" -ForegroundColor Cyan
+Write-Host "Creating storage class" -ForegroundColor Yellow
+kubectl apply --filename 01_StorageClass.yml
+Write-Host "Storage class created successfully" -ForegroundColor Cyan
 
-# Write-Host "Creating Persistant Volume Claim" -ForegroundColor Yellow
-# kubectl apply --filename 02_PersistantVolumeClaim.yml
-# Write-Host "Persistant Volume Claim created successfully" -ForegroundColor Cyan
+Write-Host "Creating Persistant Volume Claim" -ForegroundColor Yellow
+kubectl apply --filename 02_PersistantVolumeClaim.yml
+Write-Host "Persistant Volume Claim created successfully" -ForegroundColor Cyan
 
-# Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
-# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB
-# kubectl apply --recursive --filename . 
+Write-Host "Deploying Tech Talks DB service" -ForegroundColor Yellow
+Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksDB
+kubectl apply --recursive --filename .
 
 Write-Host "Tech talks DB service deployed successfully" -ForegroundColor Cyan
 
-# Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
-# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
-# kubectl apply --recursive --filename . 
+Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
+Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksAPI
+kubectl apply --recursive --filename .
 
 Write-Host "Tech talks API service deployed successfully" -ForegroundColor Cyan
 
-# Write-Host "Deploying Tech Talks web frontend" -ForegroundColor Yellow
-# Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksWeb
-# kubectl apply --recursive --filename . 
+Write-Host "Deploying Tech Talks web frontend" -ForegroundColor Yellow
+Set-Location ~/projects/AKS-learning-series/k8s/AKS/TechTalksWeb
+kubectl apply --recursive --filename .
 
-# Write-Host "Tech talks web frontend deployed successfully" -ForegroundColor Cyan
+Write-Host "Tech talks web frontend deployed successfully" -ForegroundColor Cyan
 
 Write-Host "All the services related to Tech Talks application have been successfully deployed" -ForegroundColor Cyan
 
